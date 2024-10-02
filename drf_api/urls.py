19c53d219c8aa8c6e5738d89f95d162488f75a5e
Florian Hass <f91hama@gmail.com>
2024-10-02 18:39:42 +0000

    Install dj-rest-auth and add to drf_api urls

--- drf_api/urls.py
+++ drf_api/urls.py
@@ -1,4 +1,3 @@
-
 """drf_api URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -20,6 +19,10 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
+    path('dj-rest-auth/', include('dj_rest_auth.urls')),
+    path(
+        'dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')
+    ),
     path('', include('profiles.urls')),
     path('', include('posts.urls')),
     path('', include('comments.urls')),
