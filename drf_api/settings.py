84a64f98d8b3b98ed7069217c50ad64a66706c96
Florian Hass <f91hama@gmail.com>
2024-10-03 07:31:54 +0000

    Update requirements.txt

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -60,7 +60,7 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEV' in os.environ
 
 ALLOWED_HOSTS = ['8000-fgit24-djangorestframew-d03qwvidq3a.ws.codeinstitute-ide.net', 
                 'https://social-media-moments-1ce47a41709c.herokuapp.com',
